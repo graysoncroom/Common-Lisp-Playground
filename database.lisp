308c0504a022cf9c9129828a19edd0f056cb07a7
Grayson Croom <gcroom@graysons-mbp.lan>
2018-12-05 23:00:38 -0600

    Finished basic database from Practical Common Lisp. Next up reducing repeated code

--- database.lisp
+++ database.lisp
@@ -9,4 +9,72 @@
 (defun add-record (cd) (push cd *db*))
 
 (defun dump-db ()
-  (format t "~{~{~a:~10t~a~%~}~%~}"))
+  (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+	(if (not (y-or-n-p "Another? [y/n]: ")) (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+		       :direction :output
+		       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+#|
+;; specific version of select + where for artists
+(defun select-by-artist (artist)
+  (remove-if-not #'(lambda (cd)
+		     (equal (getf cd :artist) artist))
+		 *db*))
+|#
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+#|
+;; specific version of where for artists
+(defun artist-selector (artist)
+  #'(lambda (cd) (equal (getf cd :artist) artist)))
+|#
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd)
+      (and
+       (if title    (equal (getf cd :title)  title)  t)
+       (if artist   (equal (getf cd :artist) artist) t)
+       (if rating   (equal (getf cd :rating) rating) t)
+       (if ripped-p (equal (getf cd :ripped) ripped) t))))
+
+(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+  (setf *db*
+	(mapcar
+	 #'(lambda (cd)
+	     (when (funcall selector-fn cd)
+	       (if title (setf (getf cd :title) title))
+	       (if artist (setf (getf cd :artist) artist))
+	       (if rating (setf (getf cd :rating) rating))
+	       (if ripped-p (setf (getf cd :ripped) ripped)))
+	     cd)
+	 *db*)))
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
