4075a85f3e09f326a94a026818f30acb828b7dc3
Grayson Croom <grayson.croom@gmail.com>
2018-12-10 14:51:08 -0600

    Finished unit-testing-framework

--- unit-testing-framework.lisp
+++ unit-testing-framework.lisp
@@ -5,9 +5,11 @@
   (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
 |#
 
+#|
 (defun report-result (result form)
   (format t "~:[FAIL~;pass~] ... ~a~%" result form)
   result)
+|#
 
 #|
 (defun test-+ ()
@@ -26,12 +28,69 @@
   (check (= (+ -1 -3) -4)))
 |#
 
+#| value returned is just the result of the last test
 (defmacro check (&body forms)
   `(progn
      ,@(loop for f in forms collect `(report-results ,f ',f))))
+|#
 
-(defmacro test-+ ()
+(defvar *test-name* nil)
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro combine-results (&body forms)
+  "Same as && but without short-circuiting"
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defmacro check (&body forms)
+  `(combine-results
+    ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+#|
+(defun test-+ ()
+  (let ((*test-name* 'test-+))
+    (check
+      (= (+ 1 2) 3)
+      (= (+ 1 2 3) 6)
+      (= (+ -1 -3) -4))))
+
+(defun test-* ()
+  (let ((*test-name* 'test-*))
+    (check
+      (= (* 2 2) 4)
+      (= (* 3 5) 15))))
+
+(defun test-arithmetic ()
+  (combine-results
+   (test-+)
+   (test-*)))
+|#
+
+(deftest test-+ ()
   (check
     (= (+ 1 2) 3)
     (= (+ 1 2 3) 6)
     (= (+ -1 -3) -4)))
+
+(deftest test-* ()
+  (check
+    (= (* 2 2) 4)
+    (= (* 3 5) 15)))
+
+(deftest test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
