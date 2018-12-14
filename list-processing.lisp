279882957270b55219ead0d13610b5f7f06a9b3b
Grayson Croom <grayson.croom@gmail.com>
2018-12-14 14:30:59 -0600

    Finished chapter 12 and got through a majority of chapter 13.

--- list-processing.lisp
+++ list-processing.lisp
@@ -84,9 +84,6 @@ NULL - Predicate to test whether an object is NIL. Functionally equivalent
 (maplist #'print '(1 2 3 4 5 6))
 (maplist #'(lambda (lst) (print (reduce #'+ lst))) '(1 2 3 4 5 6))
 
-;; MAPCAR <--> MAPCAN <--> MAPC
-;; MAPLIST <--> MAPCON <--> MAPL
-
 #| MAPCAN and MAPCON work like MAPCAR and MAPLIST except for the 
 way they build up their result. While MAPCAR and MAPLIST build up 
 a completely new list to hold the results of the function calls, 
@@ -104,6 +101,154 @@ to the mapped functionwhile MAPCON, like MAPLIST, passes the cons cells.
 ;; MAPC is the cousin of MAPCAR and MAPCAN
 ;; while MAPL is the MAPLIST/MAPCON family.
 
+;; MAPCAR <--> MAPCAN <--> MAPC
+;; MAPLIST <--> MAPCON <--> MAPL
+
 (mapc #'+ '(1 2 3)) ; ==> (1 2 3)
 (mapc #'print '(1 2 3))
 (mapl #'print '(1 2 3))
+
+(let ((lst '(0 1 2 3 4 5 6 7 8 9)))
+  (mapcan #'(lambda (x) (if (= x 5) nil (list x))) lst))
+;; this is the same as
+(let ((lst '(0 1 2 3 4 5 6 7 8 9)))
+  (remove 10 lst))
+;; which is the same as
+(let ((lst '(0 1 2 3 4 5 6 7 8 9)))
+  (mapcon #'(lambda (x) (if (= (car x) 5) nil (list (car x)))) lst))
+
+
+#| COPY-LIST doesn't copy the sublists (1 2), (3 4), or (5 6) in the list
+((1 2) (3 4) (5 6)).
+COPY-TREE, on the other hand, makes a new cons cell for each of the
+cons cells in list and links them together in the same structure.
+|#
+
+;;; TREES
+
+;; The function SUBST, like SUBSTITUTE, takes a new item, and old item,
+;; and a tree (as opposed to a sequence), along with :key and :test keyword
+;; arguments, and it returns a new tree with the same shape as the
+;; original tree but with all instances of the old item replaced with
+;; the new item. For example:
+(subst 10 1 '(1 2 (3 2 1) ((1 1) (2 2))))
+
+#| SUBST-IF is analogous to SUBSTITUTE-IF.
+|#
+(subst-if 0 #'evenp '(1 2 (3 2 1) ((1 1) (2 2))))
+(subst-if-not 0 #'listp '(1 2 (3 2 1) ((1 1) (2 2))))
+(substitute-if #\2 #'alpha-char-p "a1b1c1d1e1f1g1h1i1j1k1l1m")
+
+;;; SETS
+
+#| Sets can also be implemented in terms of cons cells. In fact, you
+can treat any list as a set--Common Lisp provides several functions for
+performing set-theoretic operations on lists. However, you should bear in
+mind that because of the way lists are structured, these operations get less
+and less efficient the bigger the sets get.
+
+That said, using the built-in set functions makes it easy to write
+set-manipulation code. And for small sets they may well be more efficient
+than the alternatives. If profiling shows you that these functions are a
+performance bottleneck in you code, you can always replace the lists with
+sets built on top of hash tables or bit vectors.
+
+To build up a set, you can use the function ADJOIN. ADJOIN takes an item and
+a list representing a set and returns a list represeting the set containing
+the item and all the items in the original set. To determine whether the
+item is present, it must scan the list; if the item isn't found, ADJOIN
+creates a new cons cell holding the item and pointing to the original list
+and returns it. Otherwise, it returns the original list.
+
+ADJOIN also takes :key and :test keyword arguments, which are used when
+determining whether the item is present in the original list.
+Like CONS, ADJOIN has no effect on the original list--if you want to
+modify a particular list, you need to assign the value returned by
+ADJOIN to the place where the list came from. The modify macro
+PUSHNEW does this for you automatically.
+|#
+(defparameter *set* ())
+
+(adjoin 1 *set*) ; ==> (1)
+(setf *set* (adjoin 1 *set)) ; *set* ==> (1)
+(pushnew 2 *set*) ; *set* ==> (2 1)
+(pushnew 2 *set*) ; *set* ==> (2 1)
+
+#| You can test whether a given item is in a set with MEMBER and the
+related functions MEMBER-IF and MEMBER-IF-NOT. These functions are similar
+to the sequence functions FIND, FIND-IF, and FIND-IF-NOT except they can
+be used only with lists. And instead of returning the item when it's present,
+they return the cons cell containing the item--in other words, the sublist
+starting with the desired item. When the desired item isn't present in the
+list, all three functions return NIL.
+
+The remaining set-theoretic functions provide bulk operations:
+INTERSECTION, UNION, SET-DIFFERENCE, SET-EXCLUSIVE-OR. Each of these
+functions takes two lists and :key and :test keyword arguments and returns
+a new list representing the set resulting from performing the appropriate 
+set-theoretic operation on the two lists: 
+    INTERSECTION returns a list containing all the elements 
+found in both arguments.
+    UNION returns a list containing one instance of each unique element
+found in the two arguments.
+    SET-DIFFERENCE returns a list containing all the elements from the
+first argument that don't appear in the second argument.
+    SET-EXCLUSIVE-OR returns a list containing those elements appearing
+in only one or the other of the two argument lists but not in both.
+
+Each of these functions also has a recycling counterpart whose name is the
+same except with an N prefix.
+
+Finally, the function SUBSETP takes two lists and the usual :key and :test
+keyword arguments and returns true if the first list is a subset of the
+second--if every element in the first list is also present in the
+second list. The order of the elements in the lists does not matter.
+|#
+(subsetp '(3 2 1) '(1 2 3 4)) ; ==> t
+(subsetp '(1 2 3 4) '(3 2 1)) ; ==> nil
+
+;;; Lookup Tables: Alists and Plists
+
+#| In addition to trees and sets, you can build tables that map
+keys to values out of cons cells. Two flavors of cons-based lookup
+tables are comonly used, both of which I've mentioned in passing in
+previous chapters. They're association lists, also called alists, and
+property lists, also known as plists. While you wouldn't use either
+alists or plists for large tables--for that you'd use a hash table--it's
+worth knowing how to work with them both because for small tables they can
+be more efficient than hash tables and because they have some useful
+properties of their own.
+
+An alist is a data structure that maps keys to values and also supports
+reverse lookups, finding the key when given a value. Alists also support
+adding key/value mappings that shadow existing mappings in such a way that
+the shadowing mapping can later be removed and the original mappings
+exposed again.
+
+Under the covers, an alist is essentially a list whose elements are
+themselves cons cells. Each element can be thought of as a key/value pair
+with the key in the cons cell's CAR and the value in the CDR. For instance,
+the following is an alist mapping A to 1, B to 2, and C to 3:
+
+'((A . 1) (B . 2) (C . 3))
+
+Unless the value in the CDR is a list, cons cells representing the key/value
+pairs will be dotted pairs in s-expression notation.
+
+The main lookup function for alists is assoc, which takes a key and an
+alist and returns the first cons cell whose CAR matches they key or NIL if
+no match is found.
+|#
+(assoc 'a '((a . 1) (b . 2) (c . 3))) ; ==> (A . 1)
+(assoc 'c '((a . 1) (b . 2) (c . 3))) ; ==> (C . 3)
+(assoc 'd '((a . 1) (b . 2) (c . 3))) ; ==> nil
+
+;; To get the value corresponding to a given key, you simply pass the
+;; result of ASSOC to CDR.
+(cdr (assoc 'a '((a . 1) (b . 2) (c . 3))))
+
+#| By default the key given is compared to the keys in the alist using
+EQL, but you can change that with the standard combination of :key and
+:test keyword arguments. For instance, if you wanted to use string keys,
+you might write this: |#
+(assoc "a" '(("a" . 1) ("b" . 2) ("c" . 3)) :test #'string=)
